4e8af829aa0bd3a1b8a9139dfcfe6e16cea21385
Anthony Nassar <ajnassar@gmail.com>
2014-01-05 00:48:17 -0800

    Setup skeleton of snakeUI.js

---
+++ snakeUI.js
@@ -0,0 +1,21 @@
+(function(root){
+  var Beast = Beast.root = (Beast.root || {});
+
+  var View = Beast.View = function(){
+
+  }
+
+  View.prototype.start = function(){
+
+  }
+
+  var handleKeyEvent = funciton(event){
+
+  }
+
+  View.prototype.step = function(){
+
+  };
+
+
+})(this);
